371e2630ab456da6b60bb49e04f94229af656b01
Tarık Aydın <81830855+trkaydn@users.noreply.github.com>
2021-05-12 00:29:13 +0300

    gorev2

---
+++ MvcProjeKampi/MvcProjeKampi/BusinessLayer/ValidationRules/CategoryValidator.cs
@@ -0,0 +1,21 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class CategoryValidator : AbstractValidator<Category>
+    {
+        public CategoryValidator()
+        {
+            RuleFor(x => x.CategoryName).NotEmpty().WithMessage("Kategori adı boş geçilemez");
+            RuleFor(x => x.CategoryDescription).NotEmpty().WithMessage("Açıklama boş geçilemez");
+            RuleFor(x => x.CategoryName).MinimumLength(3).WithMessage("En az üç karakter olmalıdır");
+            RuleFor(x => x.CategoryName).MaximumLength(20).WithMessage("En fazla yirmi karakter olmalıdır");
+        }
+    }
+}
